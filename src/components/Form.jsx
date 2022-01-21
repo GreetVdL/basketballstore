47f3a6d81a002930a54585a223c741c30fe86f86
Greet Van der Linden <vanderlindengreet@gmail.com>
2022-01-21 10:07:39 +0100

    add MUI for dropdown

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,18 @@
 import "./Form.scss";
 import { useState, useRef } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: [20, "!important"],
+  },
+});
 
 const Form = (props) => {
   const formRef = useRef();
@@ -26,16 +39,47 @@ const Form = (props) => {
   return (
     <form action="" onSubmit={handleFormSubmit} ref={formRef}>
       <h2>shop basketballs</h2>
-      <label htmlFor="brand">Brand:</label>
-      <select name="brand" id="brand" onChange={handleBrandSelection}>
+      {/* <label htmlFor="brand">Brand:</label> */}
+      {/* <select name="brand" id="brand" onChange={handleBrandSelection}>
         <option value="selectabrand">Select a brand</option>
         <option value="Spalding">Spalding</option>
         <option value="Wilson">Wilson</option>
         <option value="Molten">Molten</option>
         <option value="Baden">Baden</option>
-      </select>
+      </select> */}
+      <FormControl fullWidth>
+        <InputLabel id="brand-label" style={{ fontSize: "2rem" }}>
+          Brand
+        </InputLabel>
+        <Select
+          labelId="brand"
+          id="brand"
+          value={props.brand}
+          label="Brand"
+          onChange={handleBrandSelection}
+          style={{ fontSize: "2rem" }}
+        >
+          <MenuItem value="selectabrand" style={{ fontSize: "2rem" }}>
+            Select a brand
+          </MenuItem>
+          <MenuItem value="Spalding" style={{ fontSize: "2rem" }}>
+            Spalding
+          </MenuItem>
+          <MenuItem value="Wilson" style={{ fontSize: "2rem" }}>
+            Wilson
+          </MenuItem>
+          <MenuItem value="Molten" style={{ fontSize: "2rem" }}>
+            Molten
+          </MenuItem>
+          <MenuItem value="Baden" style={{ fontSize: "2rem" }}>
+            Baden
+          </MenuItem>
+        </Select>
+      </FormControl>
       {isValid ? null : <p className="check">Please select a brand</p>}
-      <label htmlFor="quantity">Quantity:</label>
+      <label htmlFor="quantity" className="quantity">
+        Quantity:
+      </label>
       <input
         value={props.quantity}
         type="number"
